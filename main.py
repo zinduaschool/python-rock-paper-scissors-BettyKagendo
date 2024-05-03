22736fe19c906a0364dcbb4a996dd535040ffe44
BettyKagendo <gitongabetty.k@gmail.com>
2024-05-03 14:21:54 +0300

    Create game_winner function

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 
 #import choice from python library
 from random import choice
+
+#defining a function and rules of the game
+def game_winner(computer_choice , user_choice):
+    if computer_choice == user_choice:
+        return 'it is a tie'
+    elif((computer_choice == 'rock' and user_choice == 'scissors' ) or
+        (computer_choice == 'paper' and user_choice == 'rock') or 
+        (computer_choice == 'scissors' and user_choice == 'paper')):
+       return f'computer chose {computer_choice}. computer wins!'
+    else:
+        return f'computer chose {computer_choice}. user wins!' 
