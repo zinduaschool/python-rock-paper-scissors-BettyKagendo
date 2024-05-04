ece67e74a81fdca3f3a0f4539967dac3a79837e7
BettyKagendo <gitongabetty.k@gmail.com>
2024-05-04 17:50:45 +0300

    modify game_winner function

--- main.py
+++ main.py
@@ -5,16 +5,16 @@ from random import choice
 #define the choices
 choices = ['rock', 'paper', 'scissors']
 
-#defining a function and rules of the game
+#defining game_winner function and rules of the game
 def game_winner(computer_choice , user_choice):
     if computer_choice == user_choice:
         return 'it is a tie'
     elif((computer_choice == 'rock' and user_choice == 'scissors' ) or
         (computer_choice == 'paper' and user_choice == 'rock') or 
         (computer_choice == 'scissors' and user_choice == 'paper')):
-       return f'computer chose {computer_choice}. computer wins!'
+       return 'computer wins!'
     else:
-        return f'computer chose {computer_choice}. user wins!' 
+        return 'user wins!' 
     
 #Get computer_choice
 computer_choice = choice (['rock', 'paper', 'scissors'])
